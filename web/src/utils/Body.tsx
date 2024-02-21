69293877aa5821daa21fcc9e75b4697c324dbdda
Kishan <87982626+Soulkeepertsx@users.noreply.github.com>
2024-02-22 03:55:40 +0530

    Minor fix + ox_inventory update sync

--- web/src/utils/Body.tsx
+++ web/src/utils/Body.tsx
@@ -215,13 +215,13 @@ export default function Body(
                 <div onMouseEnter={() => { setHoverData(true); setBodypart('HEAD') }} onMouseLeave={() => { setHoverData(false) }} className='headd' style={{ width: '4vw', height: '4.5vw', position: 'absolute', marginLeft: '11vw', marginTop: '1vw', zIndex: 1, borderRadius: '1vw', rotate: '2deg' }} />
                 <div onMouseEnter={() => { setHoverData(true); setBodypart('NECK') }} onMouseLeave={() => { setHoverData(false) }} className='neckk' style={{ width: '6.5vw', height: '2.5vw', position: 'absolute', marginLeft: '9.8vw', marginTop: '5.5vw', zIndex: 1, borderRadius: '1vw', rotate: '0deg' }} />
                 <div onMouseEnter={() => { setHoverData(true); setBodypart('LOWER_BODY') }} onMouseLeave={() => { setHoverData(false) }} className='lowerbodyyy' style={{ width: '6.5vw', height: '5vw', position: 'absolute', marginLeft: '9.8vw', marginTop: '13vw', zIndex: 1, borderRadius: '1vw', rotate: '2deg' }} />
-                <div onMouseEnter={() => { setHoverData(true); setBodypart('RHAND') }} onMouseLeave={() => { setHoverData(false) }} className='rarmmm' style={{ width: '2vw', height: '12vw', position: 'absolute', marginLeft: '17.2vw', marginTop: '7vw', zIndex: 1, borderRadius: '1vw', rotate: '-15deg' }} />
-                <div onMouseEnter={() => { setHoverData(true); setBodypart('LHAND') }} onMouseLeave={() => { setHoverData(false) }} className='larmmm' style={{ width: '2vw', height: '12vw', position: 'absolute', marginLeft: '7vw', marginTop: '7.5vw', zIndex: 1, borderRadius: '1vw', rotate: '20deg' }} />
+                <div onMouseEnter={() => { setHoverData(true); setBodypart('RARM') }} onMouseLeave={() => { setHoverData(false) }} className='rarmmm' style={{ width: '2vw', height: '12vw', position: 'absolute', marginLeft: '17.2vw', marginTop: '7vw', zIndex: 1, borderRadius: '1vw', rotate: '-15deg' }} />
+                <div onMouseEnter={() => { setHoverData(true); setBodypart('LARM') }} onMouseLeave={() => { setHoverData(false) }} className='larmmm' style={{ width: '2vw', height: '12vw', position: 'absolute', marginLeft: '7vw', marginTop: '7.5vw', zIndex: 1, borderRadius: '1vw', rotate: '20deg' }} />
                 <div onMouseEnter={() => { setHoverData(true); setBodypart('LLEG') }} onMouseLeave={() => { setHoverData(false) }} className='lleggg' style={{ width: '4vw', height: '14vw', position: 'absolute', marginLeft: '8vw', marginTop: '18.5vw', zIndex: 1, borderRadius: '1vw', rotate: '0deg' }} />
                 <div onMouseEnter={() => { setHoverData(true); setBodypart('RLEG') }} onMouseLeave={() => { setHoverData(false) }} className='rlegg' style={{ width: '3.5vw', height: '16vw', position: 'absolute', marginLeft: '12.7vw', marginTop: '18.5vw', zIndex: 1, borderRadius: '1vw', rotate: '-5deg' }} />
-                <div onMouseEnter={() => { setHoverData(true); setBodypart('LARM') }} onMouseLeave={() => { setHoverData(false) }} className='lpalmmm' style={{ width: '2vw', height: '3vw', position: 'absolute', marginLeft: '3.8vw', marginTop: '18.5vw', zIndex: 1, borderRadius: '1vw', rotate: '20deg' }} />
+                <div onMouseEnter={() => { setHoverData(true); setBodypart('LHAND') }} onMouseLeave={() => { setHoverData(false) }} className='lpalmmm' style={{ width: '2vw', height: '3vw', position: 'absolute', marginLeft: '3.8vw', marginTop: '18.5vw', zIndex: 1, borderRadius: '1vw', rotate: '20deg' }} />
                 <div onMouseEnter={() => { setHoverData(true); setBodypart('LFOOT') }} onMouseLeave={() => { setHoverData(false) }} className='lfoottt' style={{ width: '2vw', height: '5vw', position: 'absolute', marginLeft: '10.8vw', marginTop: '31.8vw', zIndex: 1, borderRadius: '1vw', rotate: '30deg' }} />
-                <div onMouseEnter={() => { setHoverData(true); setBodypart('RARM') }} onMouseLeave={() => { setHoverData(false) }} className='rpalmmm' style={{ width: '2vw', height: '4vw', position: 'absolute', marginLeft: '18.8vw', marginTop: '18.5vw', zIndex: 1, borderRadius: '1vw', rotate: '-10deg' }} />
+                <div onMouseEnter={() => { setHoverData(true); setBodypart('RHAND') }} onMouseLeave={() => { setHoverData(false) }} className='rpalmmm' style={{ width: '2vw', height: '4vw', position: 'absolute', marginLeft: '18.8vw', marginTop: '18.5vw', zIndex: 1, borderRadius: '1vw', rotate: '-10deg' }} />
                 <div onMouseEnter={() => { setHoverData(true); setBodypart('RFOOT') }} onMouseLeave={() => { setHoverData(false) }} className='rfoottt' style={{ width: '3vw', height: '3vw', position: 'absolute', marginLeft: '13vw', marginTop: '34.5vw', zIndex: 1, borderRadius: '1vw', rotate: '10deg' }} />
                 <Arrowright styles={{
                     width: '10vw',
@@ -229,45 +229,45 @@ export default function Body(
                     position: 'absolute',
                     marginLeft: '11vw',
                     marginTop: '2.5vw',
-                }} durablitystyles={{width:'5vw', marginLeft:'15.5vw', marginTop:'1vw', position:'absolute'}} label={'Head'} percent={100-bodydamagecal["HEAD"]?.percent} />
+                }} durablitystyles={{ width: '5vw', marginLeft: '15.5vw', marginTop: '1vw', position: 'absolute' }} label={'Head'} percent={100 - bodydamagecal["HEAD"]?.percent} />
                 <Arrowright styles={{
                     width: '10vw',
                     height: '1.8vw',
                     position: 'absolute',
                     marginLeft: '15vw',
                     marginTop: '9vw',
-                }} durablitystyles={{width:'5vw', marginLeft:'19.5vw', marginTop:'7.5vw', position:'absolute'}} label={'Right Hand'} percent={100-bodydamagecal["RHAND"]?.percent} />
+                }} durablitystyles={{ width: '5vw', marginLeft: '19.5vw', marginTop: '7.5vw', position: 'absolute' }} label={'Right Arm'} percent={100 - bodydamagecal["RARM"]?.percent} />
                 <Arrowright styles={{
                     width: '10vw',
                     height: '1.8vw',
                     position: 'absolute',
                     marginLeft: '12vw',
                     marginTop: '26vw',
-                }} durablitystyles={{width:'5vw', marginLeft:'16.5vw', marginTop:'24.5vw', position:'absolute'}} label={'Right Leg'} percent={100-bodydamagecal["RLEG"]?.percent} />
+                }} durablitystyles={{ width: '5vw', marginLeft: '16.5vw', marginTop: '24.5vw', position: 'absolute' }} label={'Right Leg'} percent={100 - bodydamagecal["RLEG"]?.percent} />
                 <Arrowleft styles={{
                     width: '10vw',
                     height: '1.8vw',
                     position: 'absolute',
                     marginLeft: '1.2vw',
                     marginTop: '26vw',
-                }} durablitystyles={{width:'5vw', marginLeft:'1.7vw', marginTop:'24.5vw', position:'absolute'}} label={'Left Leg'} percent={100-bodydamagecal["LLEG"]?.percent} />
+                }} durablitystyles={{ width: '5vw', marginLeft: '1.7vw', marginTop: '24.5vw', position: 'absolute' }} label={'Left Leg'} percent={100 - bodydamagecal["LLEG"]?.percent} />
                 <Arrowleft styles={{
                     width: '10vw',
                     height: '1.8vw',
                     position: 'absolute',
                     marginLeft: '1.2vw',
                     marginTop: '9vw',
-                }} durablitystyles={{width:'5vw', marginLeft:'1.8vw', marginTop:'7.5vw', position:'absolute'}} label={'Right Hand'} percent={100-bodydamagecal["RHAND"]?.percent} />
+                }} durablitystyles={{ width: '5vw', marginLeft: '1.8vw', marginTop: '7.5vw', position: 'absolute' }} label={'Left Arm'} percent={100 - bodydamagecal["LARM"]?.percent} />
                 <img className="UPPERBODY" style={{ filter: `sepia(${bodydamagecal["UPPER_BODY"]?.percent}%) saturate(300%) brightness(100%) hue-rotate(299deg)` }} alt="Upperbody" src={isEnvBrowser() ? iupperbody : getItemUrl('upperbody')} />
                 <img className="img" style={{ filter: `sepia(${bodydamagecal["LOWER_BODY"]?.percent}%) saturate(300%) brightness(100%) hue-rotate(299deg)` }} alt="Lowerbody" src={isEnvBrowser() ? ilowerbody : getItemUrl('lowerbody')} />
                 <img className="LLEG" style={{ filter: `sepia(${bodydamagecal["LLEG"]?.percent}%) saturate(300%) brightness(100%) hue-rotate(299deg)` }} alt="Lleg" src={isEnvBrowser() ? illeg : getItemUrl('LLEG')} />
                 <img className="RLEG" style={{ filter: `sepia(${bodydamagecal["RLEG"]?.percent}%) saturate(300%) brightness(100%) hue-rotate(299deg)` }} alt="Rleg" src={isEnvBrowser() ? irleg : getItemUrl('RLEG')} />
-                <img className="img" style={{ filter: `sepia(${bodydamagecal["RHAND"]?.percent}%) saturate(300%) brightness(100%) hue-rotate(299deg)` }} alt="Rhand" src={isEnvBrowser() ? irarm : getItemUrl('rarm')} />
-                <img className="LARM" style={{ filter: `sepia(${bodydamagecal["LHAND"]?.percent}%) saturate(300%) brightness(100%) hue-rotate(299deg)` }} alt="Larm" src={isEnvBrowser() ? ilarm : getItemUrl('larm')} />
-                <img className="img" style={{ filter: `sepia(${bodydamagecal["LARM"]?.percent}%) saturate(300%) brightness(100%) hue-rotate(299deg)` }} alt="Lparm" src={isEnvBrowser() ? ilpalm : getItemUrl('lpalm')} />
+                <img className="img" style={{ filter: `sepia(${bodydamagecal["RARM"]?.percent}%) saturate(300%) brightness(100%) hue-rotate(299deg)` }} alt="Rhand" src={isEnvBrowser() ? irarm : getItemUrl('rarm')} />
+                <img className="LARM" style={{ filter: `sepia(${bodydamagecal["LARM"]?.percent}%) saturate(300%) brightness(100%) hue-rotate(299deg)` }} alt="Larm" src={isEnvBrowser() ? ilarm : getItemUrl('larm')} />
+                <img className="img" style={{ filter: `sepia(${bodydamagecal["LHAND"]?.percent}%) saturate(300%) brightness(100%) hue-rotate(299deg)` }} alt="Lparm" src={isEnvBrowser() ? ilpalm : getItemUrl('lpalm')} />
                 <img className="neck" style={{ filter: `sepia(${bodydamagecal["NECK"]?.percent}%) saturate(300%) brightness(100%) hue-rotate(299deg)` }} alt="Neck" src={isEnvBrowser() ? ineck : getItemUrl('neck')} />
                 <img className="head" style={{ filter: `sepia(${bodydamagecal["HEAD"]?.percent}%) saturate(300%) brightness(100%) hue-rotate(299deg)` }} alt="Head" src={isEnvBrowser() ? ihead : getItemUrl('head')} />
-                <img className="RARM" style={{ filter: `sepia(${bodydamagecal["RARM"]?.percent}%) saturate(300%) brightness(100%) hue-rotate(299deg)` }} alt="Rarm" src={isEnvBrowser() ? irpalm : getItemUrl('rpalm')} />
+                <img className="RARM" style={{ filter: `sepia(${bodydamagecal["RHAND"]?.percent}%) saturate(300%) brightness(100%) hue-rotate(299deg)` }} alt="Rarm" src={isEnvBrowser() ? irpalm : getItemUrl('rpalm')} />
                 <img className="img-2" style={{ filter: `sepia(${bodydamagecal["RFOOT"]?.percent}%) saturate(300%) brightness(100%) hue-rotate(299deg)` }} alt="RFoot" src={isEnvBrowser() ? irfoot : getItemUrl('rfoot')} />
                 <img className="img-3" style={{ filter: `sepia(${bodydamagecal["LFOOT"]?.percent}%) saturate(300%) brightness(100%) hue-rotate(299deg)` }} alt='LFoot' src={isEnvBrowser() ? ilfoot : getItemUrl('lfoot')} />
             </div>
